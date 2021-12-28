17840796bfb9d50ff63b7662563dd755a4acfa42
Hariz Halidi <hariz.halidi@gmail.com>
2021-12-28 20:26:12 +0800

    reverseString function done & all test passed

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
 const reverseString = function(str) {
-
+  const charArray = str.split('');
+  const reverseCharArray = [];
+  for (char of charArray) {
+    reverseCharArray.unshift(char);
+  }
+  return reverseCharArray.join('');
 };
 
 // Do not edit below this line
