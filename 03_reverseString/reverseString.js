a856dd36f841f1fa330d686ebc69ce79e5ce26b7
Hariz Halidi <hariz.halidi@gmail.com>
2021-12-28 20:57:53 +0800

    modified reverseString function implementation

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,10 +1,6 @@
 const reverseString = function(str) {
   const charArray = str.split('');
-  const reverseCharArray = [];
-  for (char of charArray) {
-    reverseCharArray.unshift(char);
-  }
-  return reverseCharArray.join('');
+  return charArray.reverse().join('');
 };
 
 // Do not edit below this line
