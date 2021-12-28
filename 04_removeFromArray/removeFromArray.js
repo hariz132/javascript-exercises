cdd56505b94f86fc5f4be1965ff991e774caff88
Hariz Halidi <hariz.halidi@gmail.com>
2021-12-28 23:37:32 +0800

    removeFromArray function done & all tests passed

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,12 @@
-const removeFromArray = function() {
-
-};
+const removeFromArray = function(array, ...args) {
+  for (i = 0; i < array.length; i++) {
+    if (args.some(x => x === array[i])) { // checks if array element matches any of args
+      array.splice(i, 1);
+      i--;
+    }
+  }
+  return array;
+}
 
 // Do not edit below this line
 module.exports = removeFromArray;
