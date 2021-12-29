b553cff9958c0761b5f4ceb322906879540d9a34
Hariz Halidi <hariz.halidi@gmail.com>
2021-12-29 12:05:45 +0800

    leapYears function done & all tests passed

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,9 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+  if (!(year % 4)) {
+    if (!(year % 100) && year % 400) return false; // if divisible by 100, and not by 400
+    return true;
+  } 
+  return false;
 };
 
 // Do not edit below this line
