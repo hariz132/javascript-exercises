7d66838cda7e40e68aa372f17b3f4f29591d9244
Hariz Halidi <hariz.halidi@gmail.com>
2021-12-29 14:39:13 +0800

    ftoc & ctof functions done & all tests passed

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,13 @@
-const ftoc = function() {
-
+const ftoc = function(f) {
+  let c = (f - 32) * 5 / 9;
+  return Math.round(c * 10) / 10;
+  return +(Math.round(c + 'e+1') + 'e-1'); // alternative way using exponentiation
 };
 
-const ctof = function() {
-
+const ctof = function(c) {
+  let f = c * 9 / 5 + 32;
+  return Math.round(f * 10) / 10;
+  return +(Math.round(f + 'e+1') + 'e-1'); // alternative way using exponentiation
 };
 
 // Do not edit below this line
